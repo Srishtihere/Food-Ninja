f4104a36bd5da5027e2b68c1cfb86381a3b0238e
srishti Majumder <srishtimajumder2002@gmail.com>
2021-12-29 23:30:26 +0530

    Adding new files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+  },
+  plugins: [],
+}
